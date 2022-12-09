0701effaab46354ce666e52eaf7b2b49a40eaf29
Luca Martinelli <martinelliluca98@gmail.com>
2022-12-09 19:09:25 +0100

    Fixed

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
 
 <head class="flex p-5 mb-3 items-center print:hidden justify-between">
 	<a href="/"><Logo size="40px" color="currentColor" /></a>
-	<nav class="flex flex-1 justify-center gap-10 shadow-lg sm:shadow-none sm:gap-5 fixed bottom-0 bg-white p-5 inset-x-0 z-10 sm:relative sm:bg-transparent sm:p-0">
+	<nav class="flex flex-1 justify-center gap-10 shadow-lg sm:shadow-none sm:gap-5 fixed bottom-0 bg-white dark:bg-black p-5 inset-x-0 z-10 sm:relative sm:bg-transparent sm:p-0">
 		<NavbarItem src="/" title="Home" />
 		<NavbarItem src="/ricette" title="Ricette" />
 		<NavbarItem src="/cerca" title="Scopri" />
