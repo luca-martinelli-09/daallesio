184e5872dfd26f869a12a56ddba8478ff74a2bf4
Luca Martinelli <martinelliluca98@gmail.com>
2024-06-07 22:40:44 +0200

    🐛

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,19 +1,9 @@
 import { env } from "$env/dynamic/private";
-import { dev } from "$app/environment";
-import { PrismaClient } from "@prisma/client/edge";
-import { PrismaClient as PrismaClientEdgge } from "@prisma/client";
+import { PrismaClient as PrismaClientEdgge } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = dev
-  ? new PrismaClient({
-      datasources: {
-        db: {
-          url: env.DIRECT_DATABASE_URL,
-        },
-      },
-    })
-  : new PrismaClientEdgge({
-      datasourceUrl: env.DATABASE_URL,
-    }).$extends(withAccelerate());
+const prisma = new PrismaClientEdgge({
+  datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate());
 
 export { prisma };
