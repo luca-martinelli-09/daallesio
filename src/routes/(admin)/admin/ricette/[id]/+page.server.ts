c1d08c6e2146e6f422ffa642638a0557ca2d66b2
Luca Martinelli <martinelliluca98@gmail.com>
2024-06-20 16:31:39 +0200

    test

--- src/routes/(admin)/admin/ricette/[id]/+page.server.ts
+++ src/routes/(admin)/admin/ricette/[id]/+page.server.ts
@@ -152,7 +152,7 @@ export const actions: Actions = {
         });
       });
 
-      return formData.ingredientGroups;
+      return message(form, formData.ingredientGroups);
     }
 
     // Update sources
