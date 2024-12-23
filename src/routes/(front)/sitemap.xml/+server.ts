5862d33ced63c201ef18064abc7af8385222f3da
Luca Martinelli <martinelliluca98@gmail.com>
2024-12-23 21:09:16 +0100

    🐛

--- src/routes/(front)/sitemap.xml/+server.ts
+++ src/routes/(front)/sitemap.xml/+server.ts
@@ -27,16 +27,61 @@ export const GET: RequestHandler = async ({ url }) => {
     return tags;
   }, {} as Record<string, boolean>);
 
-  const collectionsXml = collections.map((collection) => `<url><loc>${baseSite}/raccolte/${collection.slug}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`);
-  const categoriesXml = categories.map((category) => `<url><loc>${baseSite}/categorie/${category.slug}</loc><changefreq>daily</changefreq><priority>0.5</priority></url>`);
-  const areasXml = Object.entries(OriginAreaEnum).map(([_, title]) => `<url><loc>${baseSite}/aree/${slugify(title)}</loc><changefreq>daily</changefreq><priority>0.4</priority></url>`);
-  const recipesXml = categories.map((recipe) => `<url><loc>${baseSite}/ricette/${recipe.slug}</loc><changefreq>weekly</changefreq><priority>0.9</priority></url>`);
-  const tagsXml = Object.entries(allTags).map(([tag, _]) => `<url><loc>${encodeURI(`${baseSite}/tag/${tag}`)}</loc><changefreq>daily</changefreq><priority>0.3</priority></url>`);
+  const collectionsXml = collections.map(
+    (collection) => `<url>
+  <loc>${baseSite}/raccolte/${collection.slug}</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.7</priority>
+</url>`
+  );
+
+  const categoriesXml = categories.map(
+    (category) => `<url>
+  <loc>${baseSite}/categorie/${category.slug}</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.5</priority>
+</url>`
+  );
+
+  const areasXml = Object.entries(OriginAreaEnum).map(
+    ([_, title]) => `<url>
+  <loc>${baseSite}/aree/${slugify(title)}</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.4</priority>
+</url>`
+  );
+
+  const recipesXml = categories.map(
+    (recipe) => `<url>
+  <loc>${baseSite}/ricette/${recipe.slug}</loc>
+  <changefreq>weekly</changefreq>
+  <priority>0.9</priority>
+</url>`
+  );
+
+  const tagsXml = Object.entries(allTags).map(
+    ([tag, _]) => `<url>
+  <loc>${encodeURI(`${baseSite}/tag/${tag}`)}</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.3</priority>
+</url>`
+  );
 
   const response = new Response(
     `<?xml version="1.0" encoding="UTF-8" ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${baseSite}</loc><changefreq>daily</changefreq><priority>1.0</priority></url>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  <url>
+    <loc>${baseSite}</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
   ${categoriesXml.join("")}
   ${collectionsXml.join("")}
   ${areasXml.join("")}
